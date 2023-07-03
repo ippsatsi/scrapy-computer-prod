3e073403894d8b042f33e6c70f90508aa203fc76
ippsatsi <laz133@gmail.com>
2023-07-03 01:32:32 -0500

    set CONCURRENT_REQUESTS_PER_IP

--- scrapy_app/scrapy_app/settings.py
+++ scrapy_app/scrapy_app/settings.py
@@ -42,7 +42,7 @@ ROBOTSTXT_OBEY = False
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_IP = 1
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
