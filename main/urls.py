0aab04bc02dcd9803b64f62f287592489b6baea3
Luis Aguilar <laz133@example.com>
2023-07-22 20:20:30 -0500

    preparing proveedor reports

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ListaProductos, BuscarProductos
+from .views import ListaProductos, BuscarProductos, ListaProductosProveedor
 
 urlpatterns = [
    # path("", Home.as_view(), name="home"),
     path("", ListaProductos.as_view(), name="lista_productos"),
     path("buscar/", BuscarProductos.as_view(), name="buscar_productos"),
+    path("proveedor/", ListaProductosProveedor.as_view(), name="lista_productosxproveedor"),
 ]
--- main/views.py
+++ main/views.py
@@ -56,3 +56,23 @@ def get_num_prod_by_proveedor():
     # <QuerySet [{'proveedor': 'memorykings', 'num_prod': 521}, {'proveedor': 'itstore', 'num_prod': 199}, {'proveedor': 'infotec', 'num_prod': 484}]>
     prod_x_proveedor = Producto.objects.filter(fecha=date.today()).values('proveedor').annotate(num_prod=Count('titulo')).order_by('-proveedor')
     return prod_x_proveedor
+
+
+class ListaProductosProveedor(ListView):
+    model = Producto
+    paginate_by = 100
+    ordering = "-marca"
+    template_name = "buscador.html"
+
+    def get_queryset(self):
+        proveedor_q = self.request.GET.get("prov")
+        result_delete = Producto.objects.filter(fecha__lt=date.today()).delete()
+        # print(result_delete)
+        return Producto.objects.filter(proveedor=proveedor_q).order_by('-marca', '-precio_dolares')
+    
+    def get_context_data(self, **kwargs):
+        context = super(ListaProductosProveedor, self).get_context_data(**kwargs)
+
+        context["prodXproveedor"] = get_num_prod_by_proveedor()
+
+        return context
